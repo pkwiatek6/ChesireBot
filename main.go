ce42ce7042e0ada62504d382f6a290a928f519df
pkwiatek6 <pkwiatek6@gmail.com>
2024-02-06 20:21:42 -0500

    started work on bot

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/rs/zerolog/log"
 )
 
-func main() {
+var (
+	discord *discordgo.Session
+	// Token for the bot
+	Token          = flag.String("t", "", "Bot acess token")
+	GuildID        = flag.String("GID", "", "Test Guild ID. IF not passed - bot registers commands globally")
+	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
+)
+
+func init() {
+	var err error
 	discord, err := discordgo.New("Bot " + "authentication token")
 	if err != nil {
-		log.Fatal().Msgf("Unable to connect to bot: %v", err)
+		log.Fatal().Msgf("Error creating Discord session: %v", err)
 	}
+	log.Debug().Msg("Cibbectuib to Discord established")
+}
+
+func main() {
+
 }
